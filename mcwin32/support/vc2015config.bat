4a417016f05fb315a745cc27375319f17df7ed10
adamyg <adamyg@gmail.com>
2023-02-05 13:54:37 +0800

    INNO parameterized

--- mcwin32/support/vc2015config.bat
+++ mcwin32/support/vc2015config.bat
@@ -2,11 +2,15 @@
 rem
 rem Microsoft Visual Studio C/C++ 2015
 rem
+echo Configure: Microsoft Visual Studio C/C++ 2015
 if not defined GNUWIN32 (
         set GNUWIN32=\devl\gnuwin32
 )
 if not defined PERL (
         set PERL=perl
 )
-%PERL% makelib.pl --busybox=./support/busybox "--inno=C:/Program Files (x86)/Inno Setup 5/iscc" vc2015 %1 %2 %3 %4
+if not defined INNO (
+        set INNO="C:/Program Files (x86)/Inno Setup 5/iscc"
+)
+%PERL% makelib.pl --busybox=./support/busybox --inno=%INNO% vc2015 %1 %2 %3 %4
 
