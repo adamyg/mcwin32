80353bd706af0eadb5c406bed84389d869dd26ac
adamyg <adamyg@gmail.com>
2024-03-03 21:50:52 +0800

    drive expansion, direct exec of shell ops

--- mcwin32/src/win32_utl.c
+++ mcwin32/src/win32_utl.c
@@ -271,7 +271,7 @@ static void
 set_tmpdir(void)
 {
     const char *tmpdir = NULL;
-    
+
     if (NULL != (tmpdir = getenv("MC_TMPDIR")) && *tmpdir)  {
         return;
     }
@@ -392,7 +392,7 @@ mc_get_locale(void)
 {
     static char x_lang[64] = {0};
 
-    if (0 == x_lang[0]) 
+    if (0 == x_lang[0])
     {
         char iso639[16] = {0}, iso3166[16] = {0};
         LCID lcid = GetThreadLocale();
@@ -666,9 +666,9 @@ mc_USERCONFIGDIR(const char *subdir)
         const char *env;
         int len, done = FALSE;
 
-#if defined(MC_HOMEDIR_XDG) 
+#if defined(MC_HOMEDIR_XDG)
         /* <XDG_CONFIG_HOME>/mc */
-        if (NULL != (env = getenv("XDG_CONFIG_HOME")) && *env) { 
+        if (NULL != (env = getenv("XDG_CONFIG_HOME")) && *env) {
             if (0 == _access(env, 0)) {
                 _snprintf(x_buffer, sizeof(x_buffer), "%s/mc", env);
                 mkdir(x_buffer, S_IRWXU);       /* auto create */
@@ -680,7 +680,7 @@ mc_USERCONFIGDIR(const char *subdir)
 
         /* <HOME>/.config/mc */
         if (!done) { //
-            if (NULL != (env = getenv("HOME")) && *env) { 
+            if (NULL != (env = getenv("HOME")) && *env) {
                 _snprintf(x_buffer, sizeof(x_buffer), "%s/.config", env);
                 if (0 == _access(x_buffer, 0)) {
                     _snprintf(x_buffer, sizeof(x_buffer), "%s/.config/mc", env);
@@ -1077,7 +1077,7 @@ system_impl (int flags, const char *shell, const char *cmd)
                      const int cmdlen = space - cmd;
                      unsigned i;
 
-                     for (i = 0; i < _countof(busyboxexts); ++i)  {
+                     for (i = 0; i < _countof(busyboxexts); ++i) {
                          if (0 == strncmp(busyboxexts[i], cmd, cmdlen)) {
                              char *t_cmd;
 
@@ -1111,7 +1111,7 @@ system_impl (int flags, const char *shell, const char *cmd)
             } else {
                  /*
                   *  If <cmd> </bin/sh ...>
-                  *  execute as <shell> <busybox sh ...>
+                  *  execute as <busybox sh ...>
                   */
                  const char *space;
 
@@ -1120,7 +1120,7 @@ system_impl (int flags, const char *shell, const char *cmd)
                     char *t_cmd;
 
                     if (NULL != (t_cmd = g_strconcat("\"", busybox, "\" sh", space, NULL))) {
-                        ret = w32_shell(shell, t_cmd, NULL, NULL, NULL);
+                        ret = w32_shell(busybox, t_cmd, NULL, NULL, NULL);
                         g_free(t_cmd);
                     }
                     return ret;
@@ -1149,12 +1149,7 @@ system_impl (int flags, const char *shell, const char *cmd)
 
                 for (i = 0; i < _countof(busyboxcmds); ++i) {
                     if (0 == strcmp(busyboxcmds[i], argv[0])) {
-                        char *t_cmd;
-
-                        if (NULL != (t_cmd = g_strconcat("\"", busybox, "\" ", cmd, NULL))) {
-                            ret = w32_shell(shell, t_cmd, NULL, NULL, NULL);
-                            g_free(t_cmd);
-                        }
+                        ret = w32_shell(busybox, cmd, NULL, NULL, NULL);
                         return ret;
                     }
                 }
@@ -1404,7 +1399,7 @@ my_unquote(const char *cmd, int quotews)
             default: // non-whitespace
                 if (NULL == start) {
                     start = cursor;  // first non-whitespace
-                }               
+                }
                 break;
             case ' ': case '\t': // whitespace
                 if (!instring) {
@@ -1502,7 +1497,7 @@ win32_popen(const char *cmd, const char *mode)
 
     if (pe_open >= 0) {
         if (NULL == file) {
-            pe_open += _snprintf(pe_buffer, PE_BUFFER_SIZE, "popen ; %s", strerror(errno));
+            pe_open += _snprintf(pe_buffer, PE_BUFFER_SIZE, "popen : %s", strerror(errno));
 
         } else {
             HANDLE hThread;
@@ -1542,14 +1537,14 @@ win32_pclose(FILE *file)
  *  text is prepended to the error message from the pipe
  */
 void
-open_error_pipe(void)
+error_pipe_open(void)
 {
     pe_open = 0;                                // open stream
 }
 
 
 int
-close_error_pipe(int error, const char *text)
+error_pipe_close(int error, const char *text)
 {
     const char *title;
     int len;
@@ -1757,8 +1752,12 @@ system_bustargs(char *cmd, const char **argv, int cnt)
 char *
 tilde_expand(const char *directory)
 {
+    if (0 == directory[0]) {
+        return g_strdup ("");                   /* empty */
+    }
+
     if (PATH_SEP == directory[0] &&             /* fix '/X:', vfs work around */
-            isalpha((unsigned char)directory[1]) && ':' == directory[2]) {
+            directory[1] && ':' == directory[2] && isalpha((unsigned char)directory[1])) {
         ++directory;
     }
 
@@ -1767,7 +1766,7 @@ tilde_expand(const char *directory)
         if (PATH_SEP != directory[1] ||         /* preserve URL's (//<server) */
                 0 == directory[2] || PATH_SEP == directory[2]) {
             const char *cwd = vfs_get_current_dir ();
-            char path[1024];
+            char path[WIN32_PATH_MAX];
 
             if ('/' == cwd[0] && 0 == cwd[1]) { /* vfs, possible ftp/sftp */
                 if (w32_getcwd (path, sizeof(path))) {
@@ -1787,6 +1786,14 @@ tilde_expand(const char *directory)
         char *cwd = vfs_get_current_dir_n ();
         if (cwd) {                              /* . ==> <cwd> */
             return cwd;
+        }
+
+    } else if (':' == directory[1] && isalpha((unsigned char)directory[0]) &&
+                    (0 == directory[2] || ('.'== directory[2] && 0 == directory[3]))) {
+        char path[WIN32_PATH_MAX];
+
+        if (w32_getcwdd(directory[0], path, sizeof(path))) {
+            return g_strdup (path);             /* X: and X:. ==> <drive><cwd> */
         }
 
     } else if ('~' == *directory) {
@@ -1888,7 +1895,7 @@ custom_canonicalize_pathname(char *orgpath, CANON_PATH_FLAGS flags)
         for (p = lpath + 2; p[0] != '\0' && !IS_PATH_SEP (p[0]); p++)
             ;
 
-        if (p[0] == PATH_SEP && p > (orgpath + 2)) 
+        if (p[0] == PATH_SEP && p > (orgpath + 2))
         {
             if (0 == strcmp(p + 1, ".."))
             {                                   /* "//servername/.." --> "X:/" */
@@ -1907,7 +1914,7 @@ custom_canonicalize_pathname(char *orgpath, CANON_PATH_FLAGS flags)
      *  o standardize seperator
      *  o preserve leading drive
      */
-    if (!unc) 
+    if (!unc)
     {
         if (PATH_SEP == lpath[0] &&
                 ':' == lpath[2] && isalpha((unsigned char)lpath[1])) {
@@ -2129,7 +2136,7 @@ void
 canonicalize_pathname(char *path)
 {
 #if !defined(CANON_PATH_FLAGS)
-    canonicalize_pathname_custom (path, CANON_PATH_ALL);   
+    canonicalize_pathname_custom (path, CANON_PATH_ALL);
 #else
     custom_canonicalize_pathname (path, CANON_PATH_ALL);
 #endif
@@ -2183,7 +2190,7 @@ mc_build_filenamev(const char *first_element, va_list args)
 
             canonicalize_pathname (tmp_element);
             len = strlen (tmp_element);
-            start = IS_PATH_SEP (tmp_element[0]) ? tmp_element + 1 : tmp_element; 
+            start = IS_PATH_SEP (tmp_element[0]) ? tmp_element + 1 : tmp_element;
 
             g_string_append (path, start);
             if (!IS_PATH_SEP (tmp_element[len - 1]) && element != NULL)
@@ -2193,7 +2200,7 @@ mc_build_filenamev(const char *first_element, va_list args)
     }
     while (element != NULL);
 
-    if (absolute) 
+    if (absolute)
     {
         if (! path->len || ':' != path->str[1] /*not-drive*/)
         {
@@ -2206,7 +2213,7 @@ mc_build_filenamev(const char *first_element, va_list args)
                 if (driveno <= 0) driveno = w32_getlastdrive();
 
                 // see: vfs_canon() generally when we are returning from a ftp/sftp or UNC reference.
-                if (driveno > 0) 
+                if (driveno > 0)
                 {
                     char drive[3] = "X:";
                     drive[0] = driveno + ('A' - 1);
