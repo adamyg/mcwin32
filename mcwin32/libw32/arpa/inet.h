5e7697ac29568d5d3b7e4248ac81d778e7fe527f
adamyg <adamyg@gmail.com>
2024-03-15 02:56:13 +0800

    2024

--- mcwin32/libw32/arpa/inet.h
+++ mcwin32/libw32/arpa/inet.h
@@ -1,12 +1,12 @@
 #ifndef LIBW32_ARPA_INET_H_INCLUDED
 #define LIBW32_ARPA_INET_H_INCLUDED
 #include <edidentifier.h>
-__CIDENT_RCSID(gr_libw32_arpa_inet_h,"$Id: inet.h,v 1.4 2023/11/06 15:08:43 cvsuser Exp $")
+__CIDENT_RCSID(gr_libw32_arpa_inet_h,"$Id: inet.h,v 1.5 2024/03/14 18:54:32 cvsuser Exp $")
 __CPRAGMA_ONCE
 
 /* -*- mode: c; indent-width: 4; -*- */
 /*
- * Copyright (c) 1998 - 2023, Adam Young.
+ * Copyright (c) 1998 - 2024, Adam Young.
  * All rights reserved.
  *
  * This file is part of the Midnight Commander.
--- mcwin32/libw32/netinet/in.h
+++ mcwin32/libw32/netinet/in.h
@@ -1,14 +1,14 @@
 #ifndef LIBW32_NETINET_IN_H_INCLUDED
 #define LIBW32_NETINET_IN_H_INCLUDED
 #include <edidentifier.h>
-__CIDENT_RCSID(gr_libw32_netinet_in_h,"$Id: in.h,v 1.6 2023/11/06 15:08:35 cvsuser Exp $")
+__CIDENT_RCSID(gr_libw32_netinet_in_h,"$Id: in.h,v 1.7 2024/03/14 18:54:57 cvsuser Exp $")
 __CPRAGMA_ONCE
 
 /* -*- mode: c; indent-width: 4; -*- */
 /*
  *  netinet/in.h
  *
- * Copyright (c) 1998 - 2023, Adam Young.
+ * Copyright (c) 1998 - 2024, Adam Young.
  * All rights reserved.
  *
  * This file is part of the Midnight Commander.
