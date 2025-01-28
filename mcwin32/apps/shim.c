a145dfd0e833cc741bdb5b5ef965242c86093962
adamyg <adamyg@gmail.com>
2025-01-29 00:01:28 +0800

    #78 - mcdiff, mcedit and mcview

---
+++ mcwin32/apps/shim.c
@@ -0,0 +1,204 @@
+/*
+ * libshim -- application shim
+ *
+ * Copyright (c) 2024 - 2025, Adam Young.
+ * All rights reserved.
+ *
+ * This file is part of WIN32 Midnight Commander.
+ *
+ * The applications are free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, and must be distributed with the license document above.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, and must include the license document above in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * License for more details.
+ * ==end==
+ */
+
+#include "shim.h"
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0501
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef _countof
+#define _countof(__type) (sizeof(__type)/sizeof(__type[0]))
+#endif
+#if defined(__WATCOMC__)
+#define wcsdup(__x) _wcsdup(__x)
+#endif
+
+BOOL WINAPI CtrlHandler(DWORD ctrlType);
+
+BOOL WINAPI
+CtrlHandler(DWORD ctrlType)
+{
+    switch (ctrlType) {
+    case CTRL_C_EVENT:
+    case CTRL_CLOSE_EVENT:
+    case CTRL_LOGOFF_EVENT:
+    case CTRL_BREAK_EVENT:
+    case CTRL_SHUTDOWN_EVENT:
+        // forward to child
+        return TRUE;
+    default:
+        return FALSE;
+    }
+}
+
+
+static BOOL
+Diagnostics(void)
+{
+    const char *mcshim_diagositics = getenv("MCSHIM_DIAGNOSTICS"); // optional runtime diagnostics
+    return (mcshim_diagositics && mcshim_diagositics[0] && mcshim_diagositics[0] != '0'); // non-zero
+}
+
+
+static wchar_t *
+Basename(wchar_t *path)
+{
+    wchar_t *base = path;
+
+    for (;*path; ++path) {
+        if (*path == '\\' || *path == '/') { // path delimiter
+            base = path + 1; // next element
+        }
+    }
+    return base;
+}
+
+
+static BOOL
+CreateChild(PROCESS_INFORMATION *ppi, const wchar_t *name, const wchar_t *path, wchar_t *cmdline)
+{
+    STARTUPINFOW si = {0};
+
+    GetStartupInfoW(&si); // process information
+
+    if (! CreateProcessW(path, cmdline, NULL, NULL, TRUE, CREATE_SUSPENDED, NULL, NULL, &si, ppi)) {
+        const DWORD error = GetLastError();
+        wchar_t *message = NULL;
+
+        if (FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                NULL, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR) &message, 0, NULL) && message) {
+
+            wchar_t *nl, *rt; // trailing newline/return
+
+            nl = wcsrchr(message, '\n'); // trim
+            rt = wcsrchr(message, '\r'); // trim
+
+            if (nl) *nl = 0;
+            if (rt) *rt = 0;
+
+            wprintf(L"%s: unable to execute child : %s (0x%08lx)\n", name, message, error);
+
+        } else {
+            wprintf(L"%s: unable to execute child : 0x%08lx.\n", name, error);
+        }
+
+        LocalFree(message);
+        return FALSE;
+    }
+    return TRUE;
+}
+
+
+static wchar_t orgpath[1024] = {0};
+static wchar_t newpath[1024] = {0};
+
+/*
+ *  ApplicationShim - application execution redirect shim.
+ *
+ *  Parameters:
+ *      name - Application name.
+ *      alias - Replacement application name, same path assumed.
+ *
+ *  Returns:
+ *      On success no-return, otherwise returns on error.
+ */
+void
+ApplicationShim(const wchar_t *name, const wchar_t *alias)
+{
+    const BOOL diagositics = Diagnostics(); // optional runtime diagnostics
+    const unsigned aliassz = wcslen(alias), // alias length, in characters.
+        pathsz = GetModuleFileNameW(NULL, orgpath, _countof(orgpath)); // fully qualified path.
+    wchar_t *cmdline = wcsdup(GetCommandLineW()); // original command line, cloned.
+    wchar_t *base;
+
+    HANDLE job = INVALID_HANDLE_VALUE;
+    JOBOBJECT_EXTENDED_LIMIT_INFORMATION joli = {0};
+    PROCESS_INFORMATION pi = {0};
+    DWORD excode = 0;
+
+    setvbuf(stdout, NULL, _IONBF, 0); // disable buffering
+
+    // build path
+    if (pathsz >= _countof(orgpath)) {
+        wprintf(L"%s: command line too long.\n", name);
+        return;
+    }
+
+    if (diagositics) {
+        wprintf(L"ORG: %s\n", orgpath);
+        wprintf(L"CMD: %s\n", cmdline);
+    }
+
+    wmemcpy(newpath, orgpath, pathsz);
+    if (NULL == (base = Basename(newpath)) || !*base) {
+        wprintf(L"%s: command line invalid.\n", name);
+        return;
+
+    } else if ((base + aliassz) >= (newpath + _countof(newpath))) {
+        wprintf(L"%s: command line too long.\n", name);
+        return;
+    }
+    wmemcpy(base, alias, aliassz + 1 /*NUL*/);
+
+        //wmemcpy(newpath, L".\\app.exe", 9 + 1);
+    if (diagositics) {
+        wprintf(L"NEW: %s\n", newpath);
+    }
+
+    // execute child
+    job = CreateJobObject(NULL, NULL);
+    joli.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE | JOB_OBJECT_LIMIT_SILENT_BREAKAWAY_OK;
+    SetInformationJobObject(job, JobObjectExtendedLimitInformation, &joli, sizeof(joli));
+
+    if (! CreateChild(&pi, name, newpath, cmdline)) {
+        return;
+    }
+
+    // redirect signals and monitor termination
+    SetConsoleCtrlHandler(CtrlHandler, TRUE);
+    AssignProcessToJobObject(job, pi.hProcess);
+    ResumeThread(pi.hThread);
+    CloseHandle(pi.hThread);
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    GetExitCodeProcess(pi.hProcess, &excode);
+    CloseHandle(pi.hProcess);
+    CloseHandle(job);
+
+    ExitProcess(excode);
+    //no-return
+}
+
+//end
---
+++ mcwin32/support/config_windows.pl
@@ -0,0 +1,242 @@
+#!/usr/bin/perl -w
+# -*- mode: perl; -*-
+# $Id: config_windows.pl,v 1.6 2024/05/28 11:18:48 cvsuser Exp $
+# Configure front-end for native windows targets.
+#
+
+use strict;
+use warnings 'all';
+
+use Cwd;
+use File::Which qw(which where);
+
+sub
+ProgramFiles
+{
+	my $path = $ENV{ProgramFiles};
+	$path =~ s/\\/\//g
+		if ($path);
+	return $path if ($path);
+	return "C:/Program Files";
+}
+
+##  resolve binutils
+
+my $PROGRAMFILES = ProgramFiles();
+my $olocalutils = 0;
+my $omingw = undef;
+
+foreach (@ARGV) {
+	if (/^--cfg-localutils$/) {
+		print "config_windows: utilising local utils\n";
+		$olocalutils = 1;
+
+	} elsif (/^--cfg-msys=(.*)/) {
+		$omingw = "$1/usr/bin";
+		die "config_windows: --cfg-msys=$1, invalid directory.\n"
+			if (! -d $omingw);
+		print "config_windows: using mingw=${omingw}\n";
+	}
+}
+
+
+sub
+Resolve 		# (default, apps ...)
+{
+	my $default = shift;
+	return $default
+		if ($olocalutils);
+
+	if (! defined $omingw) {
+		my $gcc = which("gcc");
+		if ($gcc) {
+			$gcc =~ s/\.exe$//i;
+			$gcc =~ s/\\/\//g;
+
+			$omingw = "";
+
+			if ($gcc =~ /^(.*)\/mingw64\/bin\/gcc$/i) {
+				$omingw = "$1/usr/bin"
+					if (-d "$1/usr/bin");
+
+			} elsif ($gcc =~ /^(.*)\/mingw64\/bin\/gcc$/i) {
+				$omingw = "$1/usr/bin"
+					if (-d "$1/usr/bin");
+			}
+
+			print "config_windows: detected mingw=${omingw}\n"
+				if ($omingw);
+		}
+	}
+
+	foreach my $app (@_) {
+
+		my $mingw_resolved = undef;
+		$mingw_resolved = "${omingw}/${app}"
+			if ($omingw && -f "${omingw}/${app}.exe");
+
+		# PATH
+		my $resolved = which $app;
+		if ($resolved) {
+			$resolved =~ s/\.exe$//i;
+			$resolved =~ s/\\/\//g;
+
+			if ($mingw_resolved && ($resolved ne $mingw_resolved)) {
+				print "config_windows: <${resolved}> and <${mingw_resolved}> found; using Mingw64 instance.\n";
+				$resolved = $mingw_resolved;
+			}
+			return $resolved;
+
+		# Mingw64 package
+		} elsif ($mingw_resolved) {
+			return $mingw_resolved;
+
+		# chocolatey import
+		} elsif (-d "C:/ProgramData/chocolatey/bin") {
+			$resolved = "C:/ProgramData/chocolatey/bin/${app}";
+			return $resolved
+				if (-f "${resolved}.exe");
+		}
+	}
+	return $default;
+}
+
+sub
+ResolveCoreUtils	# ()
+{
+	my @paths = (
+		"",                                 # PATH
+		"c:/msys64/usr",                    # MSYS installation
+		"${PROGRAMFILES}/Git/usr",          # Git for Windows
+		"c:/GnuWin32",                      # https://sourceforge.net/projects/getgnuwin32/files (legacy)
+		"C:/Program Files (x86)/GnuWin32",  # choco install gnuwin32-coreutils.install (legacy)
+		);
+	my @cmds = ("mkdir", "rmdir", "cp", "mv", "rm", "egrep", "gzip", "tar", "unzip", "zip");
+
+	foreach my $path (@paths) {
+		my $success = 1;
+		if (! $path) {
+			foreach my $app (@cmds) {
+				if (! which($app) ) {
+					$success = 0;
+					goto LAST;
+				}
+			}
+			if ($success) {
+				print "config_windows: CoreUtils=PATH\n";
+				return "";
+			}
+		} else {
+			my $bin = "${path}/bin";
+			if (-d $bin) {
+				foreach my $app (@cmds) {
+					if (! -f "${bin}/${app}") {
+						$success = 0;
+						goto LAST;
+					}
+				}
+			}
+			if ($success) {
+				print "config_windows: CoreUtils=${path}\n";
+				return $path;
+			}
+		}
+	}
+	die "config_windows: unable to determine coreutils\n";
+}
+
+my $busybox	= Resolve('./support/busybox', 'busybox');
+my $wget	= Resolve('./support/wget', 'wget');
+my $bison	= Resolve('$(D_BIN)/byacc', 'bison', 'yacc');
+my $flex	= Resolve('$(D_BIN)/flex', 'flex');
+my $coreutils	= undef;
+
+##  build command line
+
+my $cwd = getcwd;
+die "config_windows: spaces within work directory <$cwd>; rename before proceeding.\n"
+	if ($cwd =~ / /);
+
+my @options;
+my $core_symlink = 0;
+my $otarget = undef;
+my $ohelp = 0;
+
+my $script  = shift @ARGV;
+foreach (@ARGV) {
+	if (/^--busybox=(.*)$/) {
+		$busybox = $1;
+	} elsif (/^--binpath=(.*)$/) {
+		$coreutils = $1;
+	} elsif (/^--wget=(.*)$/) {
+		$wget = $1;
+	} elsif (/^--bison=(.*)$/) {
+		$bison = $1;
+	} elsif (/^--flex=(.*)$/) {
+		$flex = $1;
+	} elsif (/^--cfg-symlink$/) { # undocumented
+		# --cfg-symlink, Symlink detected coreutils to ./CoreUtils.
+		$core_symlink = 1;
+	} elsif (/^--cfg-localutils$/) {
+		# consume
+	} elsif (/^--cfg-msys=/) {
+		# consume
+	} elsif (/^--help$/) {
+		$ohelp = 1;
+	} else {
+		if (/^--/) {
+			if (/^--(.*)=(.*)$/) {
+				push @options, "--$1=\"$2\"";
+			} else {
+				push @options, $_;
+			}
+		} else {
+			die "config_windows: multiple targets, <$_> unexpected\n"
+				if ($otarget);
+			$otarget = $_;
+		}
+	}
+}
+
+if ($ohelp) {
+    print <<EOU;
+
+Usage: perl config_windows [options] <command>
+
+Options:
+
+    --cfg-localutils        Override selection of bison, flex, wget and busybox to bundled versions.
+
+    --cfg-msys=<path>       Path to msys64 installation.
+EOU
+	system "$^X ${script} --help-options";
+	exit 3;
+}
+
+$coreutils = ResolveCoreUtils()
+	if (! $coreutils);
+if ($coreutils) {
+	if ($core_symlink) {
+		if ($coreutils =~ / /) { # spaces, symlink
+			print "config_windows: CoreUtils: ./CoreUtils => ${coreutils} (symlink)\n";
+			system "mklink /J CoreUtils \"${coreutils}\""
+				if (! -d "CoreUtils/bin");
+			push @options, "--binpath=./CoreUtils/bin";
+
+		} else {
+			push @options, "--binpath=${coreutils}/bin";
+		}
+	} else {
+		die "config_windows: coreutils detected yet not in PATH, add <$coreutils/bin> before proceeding.\n";
+		return;
+	}
+}
+
+die "config_windows: target missing\n"
+	if (! $otarget);
+
+print "\n$^X ${script}\n => --busybox=\"${busybox}\" --wget=\"${wget}\" --flex=\"${flex}\" --bison=\"${bison}\" @options ${otarget}\n\n";
+
+system "$^X ${script} --busybox=\"${busybox}\" --wget=\"${wget}\" --flex=\"${flex}\" --bison=\"${bison}\" @options ${otarget}";
+
+#end
